f149a35be73476620c3e25d5107414b04b7c1574
RICH-LIU-RUIQI <rliu425@gatech.edu>
2023-01-01 18:20:40 -0500

    12.31-Update Template

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.contrib.auth import authenticate,login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    def post(self, request):
+        user_name = request.POST.get('username', "")
+        pwd = request.POST.get('password', "")
+        user = authenticate(user_name=user_name, password=pwd)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            # user not found
+            return render(request, 'login.html', {'msg': 'User is not found or incorrect passwrod'})
